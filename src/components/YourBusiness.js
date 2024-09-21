30d24126683644f6cb91e57e60621115b109d30d
Uday -- GIthub <yudaysingh09@gmail.com>
2024-09-21 22:43:32 +0530

    Updated Your Business

--- src/components/YourBusiness.js
+++ src/components/YourBusiness.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Building from '../assets/building.png'
+import edit from '../assets/edit.png'
 
 const BusinessCard = ({ gstin, legalName, tradeName }) => {
   return (
@@ -8,7 +10,7 @@ const BusinessCard = ({ gstin, legalName, tradeName }) => {
       <p>{tradeName}</p>
       <div className="mt-2 flex justify-end">
         <button className="text-sm text-gray-500 flex items-center">
-          Edit
+          <img src={edit} alt='logo' className='mr-1 w-4 mt-1'></img>edit
         </button>
       </div>
     </div>
@@ -25,23 +27,24 @@ const YourBusiness = () => {
   return (
     <div className="min-h-screen bg-gray-50 p-6">
       {/* Header */}
-      <div className="flex justify-between p-3 rounded-xl bg-blue-100 text-3xl font-bold text-blue-600 items-center mb-6">
+      <div className="flex p-3 rounded-xl bg-blue-100 text-4xl font-normal text-blue-600 items-center mb-6">
+        <img src={Building} alt='logo' className='mr-3 -mt-1 w-12'></img>
         Your Business
       </div>
 
       <div className="flex justify-between">
         <div>
-          <p className="text-2xl font-medium mb-4">Hey, here are your businesses</p>
+          <p className="text-2xl font-normal mb-4">Hey, here are your businesses</p>
         </div>
         <div>
-          <button className="bg-blue-100 text-blue-500 rounded-full px-4 text-xl py-2 flex items-center">
+          <button className="bg-blue-100 text-blue-500 rounded-xl px-4 text-xl py-2 flex items-center">
             Add More Business <strong className="text-3xl ml-2 -mt-1">+</strong>
           </button>
         </div>
       </div>
 
       {/* Business Cards Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-12 mt-8">
         {businesses.map((business, index) => (
           <BusinessCard
             key={index}
